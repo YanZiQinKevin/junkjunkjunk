af7c3141c72bee60cdb41d5bbec539373a1abb55
Yan ZiQin <yanziqin04061994@126.com>
2017-11-07 09:59:01 -0800

    post

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,27 @@ var app=express();
 var bodyParser = require('body-parser');
 var path = require ('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
-
-module.exports = router;
-router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+module.exports = router;
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+
+
 
 // router.post('/test/submit',function (req,res,next) {
 //     var id=req.body.id;
 //    //res.redirect('/test/'+id);
 //     res.send("hello " + id);
 // })
-router.post('/test/submit', function(req, res, next) {
-    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
-    var params = JSON.stringify(req.params);//if wanted parameters
-    var value_name = req.body.id;  //retrieve the data associated with name
-    res.send("hello " + value_name);
-});
+// router.post('/test/submit', function(req, res, next) {
+//     var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+//     var params = JSON.stringify(req.params);//if wanted parameters
+//     var value_name = req.body.id;  //retrieve the data associated with name
+//     res.send("hello " + value_name);
+// });
 router.post('/readNameAndRespond',function (req,res,next) {
     var body=JSON.stringify(req.body);
     var params = JSON.stringify(req.params);
